80f7b0bd2dee90dd02209357b51b66af2b10119d
Ruby Wright <43528535+RJWright25@users.noreply.github.com>
2024-03-26 14:34:30 +0200

    Amending setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     long_description=open('README.md').read(),  # Make sure you have a README.md file
     long_description_content_type="text/markdown",
     url="https://github.com/RJWright25/simpipeline_helsinki",  
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},    
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
